ab476d3fd9e707f77c268327303873f265a6b38d
AshFoster <ashfoster87@gmail.com>
2022-01-16 17:20:51 +0000

    Add RestaurantList view to views.py

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Cuisine, Restaurant, Comment, Dish
 
-# Create your views here.
+
+class RestaurantList(generic.ListView):
+    model = Restaurant
+    queryset = Restaurant.objects.filter(status=1).order_by('-created_on')
+    template_name = 'index.html'
+    paginate_by = 6
