d3aa382e76b97a4cd492f2148d27d434bb9d5517
AshFoster <ashfoster87@gmail.com>
2022-01-16 08:59:29 +0000

    Add cuisine, restaurant, comment and dish models to restaurants app

--- restaurants/models.py
+++ restaurants/models.py
@@ -1,3 +1,74 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+from django.utils.text import slugify
 
-# Create your models here.
+
+STATUS = ((0, "Draft"), (1, "Published"))
+
+
+class Cuisine(models.Model):
+    name = models.CharField(max_length=50)
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+
+class Restaurant(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=100, unique=True)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='restaurant_posts')
+    updated_on = models.DateTimeField(auto_now=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    description = models.TextField()
+    featured_image = CloudinaryField('image', default='placeholder')
+    status = models.IntegerField(choices=STATUS, default=0)
+    rating = models.IntegerField(default=0)
+    favourited = models.ManyToManyField(
+        User, related_name='restaurant_favourited', blank=True)
+    location = models.CharField(max_length=200)
+    cuisine = models.ManyToManyField(
+        Cuisine, related_name='restaurant_cuisine', blank=False)
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Restaurant, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
+
+    def number_of_favourites(self):
+        return self.favourited.count()
+
+
+class Comment(models.Model):
+    restaurant = models.ForeignKey(
+        Restaurant, on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=50)
+    email = models.EmailField()
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['created_on']
+
+    def __str__(self):
+        return f"Comment {self.content} by {self.name}"
+
+
+class Dish(models.Model):
+    name = models.CharField(max_length=50)
+    cuisine = models.ForeignKey(
+        Cuisine, on_delete=models.CASCADE, related_name='cuisine_dish')
+    restaurant = models.ManyToManyField(
+        Restaurant, related_name='restaurant_dishes', blank=False)
