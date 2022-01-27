fef09784b3769f370b92488a306f2895b5de98d8
AshFoster <ashfoster87@gmail.com>
2022-01-27 18:32:56 +0000

    Update filters section to read from database, including update to RestaurantList view

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views import generic
+from django.db.models import Count
 from .models import Cuisine, Restaurant, Comment, Dish
 
 
@@ -13,4 +14,17 @@ class RestaurantList(generic.ListView):
     model = Restaurant
     queryset = Restaurant.objects.filter(approved=True).order_by('-created_on')
     template_name = 'restaurants.html'
-    paginate_by = 6
+    paginate_by = 3
+
+    def get_context_data(self, **kwargs):
+        context = super(RestaurantList, self).get_context_data(**kwargs)
+
+        cuisine_with_restaurant_count = Cuisine.objects.filter(approved=True).annotate(restaurant_count=Count('restaurant_cuisine'))
+        cuisine_with_restaurant_count_values = cuisine_with_restaurant_count.values('restaurant_count')
+        cuisine_counter = 0
+        for cuisine in cuisine_with_restaurant_count_values:
+            cuisine_counter += cuisine['restaurant_count']
+
+        context['cuisine_count'] = cuisine_counter
+        context['cuisine_list'] = cuisine_with_restaurant_count
+        return context
