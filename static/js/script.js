6ed8123ab4969587a573e63989c67c4a4255b955
AshFoster <ashfoster87@gmail.com>
2022-02-03 14:34:37 +0000

    Add additional base templates and add_restaurant.html template

--- static/js/script.js
+++ static/js/script.js
@@ -1,8 +1,7 @@
 // Function idea came from Code Institue's Blog walkthrough project
 setTimeout(function () {
-    let messagesContainer = document.getElementById('msg-alert');
     let messages = document.getElementById('msg');
     let alert = new bootstrap.Alert(messages);
-    messagesContainer.classList.remove('mt-2', 'mt-lg-4');
+    document.getElementById('msg-alert').classList.remove('mt-2');
     alert.close();
 }, 2500);
