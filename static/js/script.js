1b612b90b26c17d60dc0803d69663b63bddfcff3
AshFoster <ashfoster87@gmail.com>
2022-02-04 12:25:46 +0000

    Add javascript to allow user to add and remove dishes on add_restaurant.html

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,4 @@
+// CREDIT
 // Function idea came from Code Institue's Blog walkthrough project
 setTimeout(function () {
     let messages = document.getElementById('msg');
@@ -5,3 +6,58 @@ setTimeout(function () {
     document.getElementById('msg-alert').classList.remove('mt-2');
     alert.close();
 }, 2500);
+// END CREDIT
+
+
+// CREDIT
+// Idea for this section of code came from 'The Dumbfounds' YouTube video:
+// https://www.youtube.com/watch?v=sE_dccbr1I4&list=PLbpAWbHbi5rNUuLTzreCl1g212G7qgzpR&index=6
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelector('#dishes-input').addEventListener('keydown', function (e) {
+        if (e.keyCode !== 13) {
+            return;
+        }
+
+        e.preventDefault();
+
+        let dishName = this.value;
+        this.value = '';
+        addNewDish(dishName);
+        updateDishesString();
+    })
+
+    function addNewDish(name) {
+        document.querySelector('#dishes-container').insertAdjacentHTML('beforeend',
+            `<li class="dish d-flex justify-content-between my-3 py-2 px-3 bg-lighter-dark-custom text-light-custom rounded-3 box-shadow-custom">
+                <span class="name text-start">${name}</span>
+                <span onclick="removeDish(this)" class="btn-remove text-end" role="button">X</span>
+            </li>`)
+    }
+});
+
+function fetchDishArray() {
+    let dishes = [];
+
+    document.querySelectorAll('.dish').forEach(function (e) {
+        let name = e.querySelector('.name').innerHTML;
+
+        if (name == '') {
+            return;
+        }
+
+        dishes.push(name);
+    })
+
+    return dishes;
+}
+
+function updateDishesString() {
+    dishes = fetchDishArray();
+    document.querySelector('input[name="dishes-string"]').value = dishes.join(',')
+}
+
+function removeDish(e) {
+    e.parentElement.remove();
+    updateDishesString();
+}
+// END CREDIT
