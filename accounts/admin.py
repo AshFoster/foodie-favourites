3201095c3153b37be860dc07963a5e85beea4f90
AshFoster <ashfoster87@gmail.com>
2022-02-11 10:12:57 +0000

    Add Profile model to accounts app

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from . models import Profile
 
-# Register your models here.
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    """Define list filter for Profile Admin objects."""
+    list_filter = ('user', 'name')
--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils.text import slugify
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+class Profile(models.Model):
+    """
+    The Profile model is linked to the Django User model and is used to
+    allow the registered user's to have a profile which they can edit.
+    """
+    user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=100, unique=True)
+    location = models.CharField(max_length=200)
+    bio = models.TextField()
+    image = CloudinaryField('image', default='default')
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.user.username)
+        super(Profile, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.user.username
