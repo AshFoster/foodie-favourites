536e6d38ecbc33346c3b16a48da353f22200543d
AshFoster <ashfoster87@gmail.com>
2022-02-11 10:14:46 +0000

    Override alluth SignupForm so a new Profile object is created when a user registers an account

--- foodiefavourites/settings.py
+++ foodiefavourites/settings.py
@@ -56,6 +56,10 @@ INSTALLED_APPS = [
 
 SITE_ID = 1
 
+ACCOUNT_FORMS = {
+'signup': 'accounts.forms.CustomSignupForm',
+}
+
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
