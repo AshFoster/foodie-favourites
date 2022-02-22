6db4e02b24a5c0052c5a0c0960b69ad805991586
AshFoster <ashfoster87@gmail.com>
2022-02-22 16:44:11 +0000

    Change debug to True for testing

--- foodiefavourites/settings.py
+++ foodiefavourites/settings.py
@@ -28,9 +28,9 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-X_FRAME_OPTIONS = 'SAMEORIGIN'
+# X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 ALLOWED_HOSTS = ['foodie-favourites.herokuapp.com', 'localhost']
 
