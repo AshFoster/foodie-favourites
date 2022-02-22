fb7b9e2ca378c5ae78d91dd19674f8ec7116d83e
AshFoster <ashfoster87@gmail.com>
2022-02-22 08:53:43 +0000

    Change js linke in base.html to include 'static'

--- foodiefavourites/settings.py
+++ foodiefavourites/settings.py
@@ -94,7 +94,6 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'django.template.context_processors.static',
             ],
         },
     },
