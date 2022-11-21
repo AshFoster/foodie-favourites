51381c165ce4f53f8d098ccdae754be0c804f8bd
AshFoster <ashfoster87@gmail.com>
2022-11-21 14:49:00 +0000

    Prepare for deployment to render.com

--- foodiefavourites/settings.py
+++ foodiefavourites/settings.py
@@ -34,6 +34,10 @@ X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 ALLOWED_HOSTS = ['foodie-favourites.herokuapp.com', 'localhost']
 
+# Add Render.com URL to allowed hosts
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 # Application definition
 
